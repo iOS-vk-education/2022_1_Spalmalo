68cad3206386ef8fa09167b16fc5e71c0d70153f
Jasur Tursunov <ztursunov@ivi.ru>
2022-05-01 22:14:56 +0300

    some features added

--- Deepfakery/Deepfakery/View/ViewController.swift
+++ Deepfakery/Deepfakery/View/ViewController.swift
@@ -10,6 +10,8 @@ import AVFoundation
 import Firebase
 import SwiftUI
 import Alamofire
+import FirebaseStorage
+
 
 class ViewController: UIViewController {
     private let topNavbar = UINavigationBar(frame: CGRect(x: 0,
@@ -29,9 +31,12 @@ class ViewController: UIViewController {
                                                          collectionViewLayout:UICollectionViewLayout.init())
     private let cameraButton = UIButton()
     private let galleryButton = UIButton()
-    private let startButton = UIButton()
+    private let clearButton = UIButton()
+    private let goButton = UIButton()
+    private let saveButton = UIButton()
     private let logOutButton = UIButton()
     private let imageView = UIImageView()
+    private let resultLabel = UILabel()
 
         
     // MARK: - Life cycle
@@ -46,7 +51,6 @@ class ViewController: UIViewController {
     private func configureUI() {
         self.addTopNavBar()
         self.addCollectionView()
-        
     }
     
     private func addTopNavBar() {
@@ -56,17 +60,122 @@ class ViewController: UIViewController {
     }
     
     
+    
     private func addCollectionView() {
         self.view.addSubview(self.collectionView)
         self.collectionView.backgroundColor = .white
+        self.addImageView()
+        self.addResultLabel()
         self.addCameraButton()
         self.addGalleryButton()
-//        self.addStartButton()
-        self.addLogOutButton()
-        self.addImageView()
-        
+        self.addClearButton()
+        self.addGoButton()
+        self.addSaveButton()
+//        self.addLogOutButton()
+               
         
     }
+    
+    private func addImageView() {
+        self.view.addSubview(self.imageView)
+        self.imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.imageView.backgroundColor = .black
+        NSLayoutConstraint.activate([
+            self.imageView.topAnchor.constraint(equalTo: self.topNavbar.bottomAnchor, constant: 20),
+            self.imageView.heightAnchor.constraint(equalToConstant: 350),
+            self.imageView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 30),
+            self.imageView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -30),
+        ])
+    }
+    
+    private func addResultLabel() {
+        self.collectionView.addSubview(self.resultLabel)
+        self.resultLabel.textColor = .black
+        self.resultLabel.font = UIFont(name: "Avenir-Light", size: 25)
+        self.resultLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.resultLabel.topAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: 20),
+            self.resultLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        ])
+    }
+
+    
+    private func addCameraButton() {
+        self.collectionView.addSubview(self.cameraButton)
+        self.cameraButton.backgroundColor = .umud
+        self.cameraButton.layer.cornerRadius = 20
+        self.cameraButton.setImage(UIImage(named: "camera.png"), for: .normal)
+        self.cameraButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.cameraButton.topAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: 60.0),
+            self.cameraButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 50),
+            self.cameraButton.widthAnchor.constraint(equalToConstant: 120),
+            self.cameraButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        self.cameraButton.addTarget(self, action: #selector(self.onClickCameraButton(sender:)), for: .touchUpInside)
+    }
+    
+    private func addGalleryButton() {
+        self.collectionView.addSubview(self.galleryButton)
+        self.galleryButton.backgroundColor = .serik
+        self.galleryButton.layer.cornerRadius = 20
+        self.galleryButton.setImage(UIImage(named: "gallery.png"), for: .normal)
+        self.galleryButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.galleryButton.topAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: 60.0),
+            self.galleryButton.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -50),
+            self.galleryButton.widthAnchor.constraint(equalToConstant: 120),
+            self.galleryButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        self.galleryButton.addTarget(self, action: #selector(self.onClickGalleryButton(sender:)), for: .touchUpInside)
+    }
+    
+    private func addClearButton() {
+        self.collectionView.addSubview(self.clearButton)
+        self.clearButton.backgroundColor = .serik
+        self.clearButton.layer.cornerRadius = 20
+        self.clearButton.setImage(UIImage(named: "trash.png"), for: .normal)
+        self.clearButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.clearButton.topAnchor.constraint(equalTo: self.cameraButton.bottomAnchor, constant: 20.0),
+            self.clearButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 50),
+            self.clearButton.widthAnchor.constraint(equalToConstant: 120),
+            self.clearButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        self.clearButton.addTarget(self, action: #selector(self.onClickClearButton(sender:)), for: .touchUpInside)
+    }
+    
+    private func addGoButton() {
+        self.collectionView.addSubview(self.goButton)
+        self.goButton.backgroundColor = .umud
+        self.goButton.layer.cornerRadius = 20
+        self.goButton.setImage(UIImage(named: "go.png"), for: .normal)
+        self.goButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.goButton.topAnchor.constraint(equalTo: self.galleryButton.bottomAnchor, constant: 20.0),
+            self.goButton.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -50),
+            self.goButton.widthAnchor.constraint(equalToConstant: 120),
+            self.goButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        self.goButton.addTarget(self, action: #selector(self.onClickGoButton(sender:)), for: .touchUpInside)
+    }
+    
+    
+    private func addSaveButton() {
+        self.collectionView.addSubview(self.saveButton)
+        self.saveButton.backgroundColor = .umud
+        self.saveButton.layer.cornerRadius = 20
+        self.saveButton.setImage(UIImage(named: "save.png"), for: .normal)
+        self.saveButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.saveButton.topAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: 200.0),
+            self.saveButton.centerXAnchor.constraint(equalTo: self.imageView.centerXAnchor),
+            self.saveButton.widthAnchor.constraint(equalToConstant: 120),
+            self.saveButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        self.saveButton.addTarget(self, action: #selector(self.onClickSaveButton(sender:)), for: .touchUpInside)
+    }
+    
     
     private func addLogOutButton() {
         self.collectionView.addSubview(self.logOutButton)
@@ -80,64 +189,54 @@ class ViewController: UIViewController {
         self.logOutButton.addTarget(self, action: #selector(self.onClickLogoutButton), for: .touchUpInside)
     }
     
-    private func addCameraButton() {
-        self.collectionView.addSubview(self.cameraButton)
-        self.cameraButton.backgroundColor = .umud
-        self.cameraButton.layer.cornerRadius = 20
-        self.cameraButton.setImage(UIImage(named: "camera.png"), for: .normal)
-        self.cameraButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            self.cameraButton.topAnchor.constraint(equalTo: self.topNavbar.bottomAnchor, constant: 80.0),
-            self.cameraButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 50),
-            self.cameraButton.widthAnchor.constraint(equalToConstant: 120),
-            self.cameraButton.heightAnchor.constraint(equalToConstant: 120)
-        ])
-        self.cameraButton.addTarget(self, action: #selector(self.onClick(sender:)), for: .touchUpInside)
+    private func getResult(image: UIImage) {
+        //https://deepfakery.herokuapp.com/api/v1/photo
+        let imageData = image.pngData()
+        AF.upload(multipartFormData: { (multipartFormData) in
+            multipartFormData.append(imageData!, withName: "data", fileName: "swift_file.png", mimeType: "image/png")
+        },
+                  to: "http://127.0.0.1:5000/api/v1/photo", method: .post).responseDecodable(of: DecodableType.self) { response in
+            switch response.result {
+            case .success(let data):
+                self.resultLabel.text = data.result.type
+            case .failure(_):
+                let alert = UIAlertController(title: "Error",
+                                              message: "Something go wrong\nTry again",
+                                              preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK",
+                                              style: .default,
+                                              handler: { _ in }))
+                
+                self.present(alert, animated: true)
+            }
+        }
     }
     
-    private func addGalleryButton() {
-        self.collectionView.addSubview(self.galleryButton)
-        self.galleryButton.backgroundColor = .serik
-        self.galleryButton.layer.cornerRadius = 20
-        self.galleryButton.setImage(UIImage(named: "gallery.png"), for: .normal)
-        self.galleryButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            self.galleryButton.topAnchor.constraint(equalTo: self.topNavbar.bottomAnchor, constant: 80.0),
-            self.galleryButton.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -50),
-            self.galleryButton.widthAnchor.constraint(equalToConstant: 120),
-            self.galleryButton.heightAnchor.constraint(equalToConstant: 120)
-        ])
-        self.galleryButton.addTarget(self, action: #selector(self.onClickGalleryButton(sender:)), for: .touchUpInside)
-    }
-    
-    private func addImageView() {
-        self.collectionView.addSubview(self.imageView)
-        self.imageView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            self.imageView.topAnchor.constraint(equalTo: self.topNavbar.bottomAnchor, constant: 250),
-            self.imageView.heightAnchor.constraint(equalToConstant: 30),
-            self.imageView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 50),
-            self.imageView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -50),
-            self.imageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -300)
-        ])
-    }
-    
-    private func addStartButton() {
-        self.collectionView.addSubview(self.startButton)
-        self.startButton.backgroundColor = .red
-        self.startButton.layer.cornerRadius = 20
-        self.startButton.setTitle("GO", for: .normal)
-        self.startButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            self.startButton.topAnchor.constraint(equalTo: self.cameraButton.bottomAnchor, constant: 50.0),
-            self.startButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 110),
-            self.startButton.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -110),
-            self.startButton.heightAnchor.constraint(equalToConstant: 40)
-        ])
+    private func upload(currentUserId: String, image: UIImage, completion: @escaping(Result<URL, Error>) -> Void) {
+        let reference = Storage.storage().reference().child(currentUserId)
+        
+//        guard let imageData = image.jpegData(compressionQuality: 0.4) else { return }
+        guard let imageData = image.pngData() else { return }
+        reference.putData(imageData, metadata: nil) { (metadata, error) in
+            guard let _ = metadata else {
+                print("ERROR = \(error!)")
+                completion(.failure(error!))
+                return
+            }
+            reference.downloadURL { (url, error) in
+                guard let url = url else {
+                    completion(.failure(error!))
+                    return
+                }
+                completion(.success(url))
+            }
+            
+        }
     }
     
     @objc
-    private func onClick(sender: UIButton) {
+    private func onClickCameraButton(sender: UIButton) {
         let picker = UIImagePickerController()
         picker.sourceType = .camera
         picker.delegate = self
@@ -145,12 +244,54 @@ class ViewController: UIViewController {
         present(picker, animated: true)
     }
     
-    @objc func onClickGalleryButton(sender: UIButton) {
+    @objc
+    func onClickGalleryButton(sender: UIButton) {
         let picker = UIImagePickerController()
         picker.sourceType = .photoLibrary
         picker.delegate = self
         picker.allowsEditing = true
         present(picker, animated: true)
+    }
+    
+    @objc
+    func onClickGoButton(sender: UIButton) {
+        guard let image = self.imageView.image else { return }
+        self.getResult(image: image)
+    }
+    
+    @objc
+    func onClickClearButton(sender: UIButton) {
+        self.resultLabel.text = ""
+        self.imageView.image = nil
+        self.imageView.backgroundColor = .black
+    }
+    
+    
+    @objc
+    func onClickSaveButton(sender: UIButton) {
+        self.upload(currentUserId: Auth.auth().currentUser!.uid, image: self.imageView.image!) {
+            (result) in
+            switch result {
+            case .success(let url):
+                let db = Firestore.firestore()
+                db.collection("user_images").addDocument(data: [
+                    "imageURL": url.absoluteString,
+                    "type": self.resultLabel.text,
+                    "uuid": Auth.auth().currentUser!.uid
+                ])
+                let alert = UIAlertController(title: "Success",
+                                              message: "Image saved",
+                                              preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK",
+                                              style: .default,
+                                              handler: { _ in }))
+                
+                self.present(alert, animated: true)
+            case .failure(_):
+                return
+            }
+        }
     }
     
     @objc
@@ -174,51 +315,12 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         guard let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage else { return }
         self.imageView.image = image
         picker.dismiss(animated: true, completion: nil)
-        print("RESULT = \(getResult(image: image))")
+        self.resultLabel.text = ""
+//        print("RESULT = \(getResult(image: image))")
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
-    }
-    
-    func getResult(image: UIImage) -> String {
-        let imageData = image.pngData()
-        AF.upload(multipartFormData: { (multipartFormData) in
-                multipartFormData.append(imageData!, withName: "data", fileName: "swift_file.png", mimeType: "image/png")
-            },
-            to: "http://127.0.0.1:8080/api/v1/photo", method: .post).responseDecodable(of: DecodableType.self) { response in
-                switch response.result {
-                case .success(let data):
-                    print("RESULT = \(data.result.type)")
-                case .failure(_):
-                    let alert = UIAlertController(title: "Error",
-                                                  message: "Something go wrong\nTry again",
-                                                  preferredStyle: .alert)
-
-                    alert.addAction(UIAlertAction(title: "OK",
-                                                  style: .default,
-                                                  handler: { _ in }))
-
-                    self.present(alert, animated: true)
-                }
-            }
-//        AF.request("http://127.0.0.1:8080/api/v1/photo", method: .post).responseDecodable(of: DecodableType.self) { response in
-//            switch response.result {
-//            case .success(let data):
-//                print(data)
-//            case .failure(_):
-//                let alert = UIAlertController(title: "Error",
-//                                              message: "Something go wrong\nTry again",
-//                                              preferredStyle: .alert)
-//
-//                alert.addAction(UIAlertAction(title: "OK",
-//                                              style: .default,
-//                                              handler: { _ in }))
-//
-//                self.present(alert, animated: true)
-//            }
-//        }
-        return "jjjj"
     }
 }
 
